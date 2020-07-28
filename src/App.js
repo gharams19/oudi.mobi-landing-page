489a5285df0f7346a3828a35031287dd70fd2519
Gharam Alsaedi <gbalsaedi@ucdavis.edu>
2020-07-28 13:32:41 -0700

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+class NavBar extends Component {
+  render() {
+    return (
+      <div id = "navbar">
+        <a href = "">Home</a>
+        <a href = "">About</a>
+        <a href = "">Subscribe</a>
+        <a href = "">Privacy and Security</a>
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar/>
     </div>
   );
 }
